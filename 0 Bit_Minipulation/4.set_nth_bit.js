089053d2805f7cfd8e4fcd82214b141d300df6ff
Sharjeel Moqrab Khan <38564835+SharjeelMoqrabKhan@users.noreply.github.com>
2021-06-11 17:36:02 +0500

    updated set bit

--- 0 Bit_Minipulation/4.set_nth_bit.js
+++ 0 Bit_Minipulation/4.set_nth_bit.js
@@ -1,7 +1,7 @@
 function setBit(number, bit) {
-  number = number | (1 << bit);
+  number = number | (1 << bit-1);
   return number;
 }
 
 let n = 10; //1010
-console.log(setBit(n, 2));
+console.log(setBit(n, 3));
